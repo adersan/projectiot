d9e367ffead558d6b86ddc7045ed016ffe30ad62
Aderval Santiago <adersan@hotmail.com>
2025-04-14 15:03:36 -0300

    fontend

---
+++ frontend/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Aqui futuramente você pode integrar o EasyOCR e trocar a imagem da placa com base no resultado
+  console.log("Interface carregada");
+});
+
+// Para futuro uso:
+// document.getElementById("placa-img").src = "placas/XYZ4321.png";
